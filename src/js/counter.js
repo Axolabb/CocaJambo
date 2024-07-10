36200c70ca50865e8ff1de5d893f44e45651c6b7
AXOLOTLI <axolotligithub@gmail.com>
2024-07-10 14:43:18 +0300

    first commit

---
+++ src/js/counter.js
@@ -0,0 +1,4 @@
+export const test = 200;
+export const func = () => {
+  console.log('function is detected ' + test + ' lol')
+}
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '../css/main.css'
+import { test, func } from './counter'
+
+console.log(test)
+func()
